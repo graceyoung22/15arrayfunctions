d96e862f9d776c9f6716d9e3329135a911e5f6f2
Grace A Young <gay5@afsconnect1.njit.edu>
2016-10-12 22:31:02 -0400

    diff key

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php
 
-array array_change_key_case ( array $array [, int $case = CASE_LOWER ] )
-
-
-$array = array( 
+$array1 = array( 
 	array('key'=>'word1', 'value'=>1),
 	array('key'=>'word2', 'value'=>2),
 	array('key'=>'word3', 'value'=>3),
 );
+
+$array2 = array(
+        array('key'=>'word4', 'value'=>4),
+        array('key'=>'word5', 'value'=>5),
+        array('key'=>'word6', 'value'=>6),
+);
+
+
+array array_change_key_case ( array $array1 [, int $case = CASE_LOWER ] )
+
+array array_diff_key ( array $array1 , array $array2 )
+
+
+
+
 
 
 ?>
