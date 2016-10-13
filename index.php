1ca38ef5fe010e31d805d709ec6f64f48aadf03d
Grace A Young <gay5@afsconnect1.njit.edu>
2016-10-12 22:21:01 -0400

    change key case

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+array array_change_key_case ( array $array [, int $case = CASE_LOWER ] )
+
+
+$array = array( 
+	array('key'=>'word1', 'value'=>1),
+	array('key'=>'word2', 'value'=>2),
+	array('key'=>'word3', 'value'=>3),
+);
+
+
+?>
+
